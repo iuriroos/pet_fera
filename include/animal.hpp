485cae2e1d881cc94a912fbc97f47172d840fda6
Westefns Souza <westefns@outlook.com.br>
2020-11-24 18:01:33 -0300

    implementação inicial do header e classe Animal

--- include/animal.hpp
+++ include/animal.hpp
@@ -1,8 +1,34 @@
 #pragma once
 
+#include <string>
+
+using namespace std;
+
 class Animal {
 public:
+    Animal();
+    Animal(string especie, int tamanho, string cor, double preco, string tipoDeAlimentacao);
+    ~Animal();
 
-private:
+    string getEspecie() const;
+    void setEspecie(string especie);
 
+    int getTamanho() const;
+    void setTamanho(int tamanho);
+    
+    string getCor() const;
+    void setCor(string cor);
+    
+    double getPreco() const;
+    void setPreco(double preco);
+    
+    string getTipoDeAlimentacao() const;
+    void setTipoDeAlimentacao(string tipoDeAlimentacao);
+
+protected:
+    string especie;
+    int tamanho;
+    string cor;
+    double preco;
+    string tipoDeAlimentacao;
 };
--- src/animal.cpp
+++ src/animal.cpp
@@ -1,1 +1,48 @@
 #include "animal.hpp"
+
+Animal::Animal() {}
+
+Animal::Animal(string especie, int tamanho, string cor, double preco, string tipoDeAlimentacao)
+    : especie(especie), tamanho(tamanho), cor(cor), preco(preco), tipoDeAlimentacao(tipoDeAlimentacao) {}
+
+Animal::~Animal() {}
+
+string Animal::getEspecie() const {
+    return this->especie;
+}
+
+void Animal::setEspecie(string especie) {
+    this->especie = especie; 
+}
+
+int Animal::getTamanho() const {
+    return this->tamanho;
+}
+
+void Animal::setTamanho(int tamanho) {
+    this->tamanho = tamanho;
+}
+    
+string Animal::getCor() const {
+    return this->cor;
+}
+
+void Animal::setCor(string cor) {
+    this->cor = cor;
+}
+
+double Animal::getPreco() const {
+    return this->preco;
+}
+
+void Animal::setPreco(double preco) {
+    this->preco = preco;
+}
+    
+string Animal::getTipoDeAlimentacao() const {
+    return tipoDeAlimentacao;
+}
+
+void Animal::setTipoDeAlimentacao(string tipoDeAlimentacao) {
+    this->tipoDeAlimentacao = tipoDeAlimentacao; 
+}
