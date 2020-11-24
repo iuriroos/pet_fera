e6bba184ff9c5201547c2f85ab0666222a969395
Westefns Souza <westefns@outlook.com.br>
2020-11-24 20:43:52 -0300

    Implementação da classe silvestre, exotica e mudando ameacadoDeExtincao para a classe silvestre

--- include/silvestre.hpp
+++ include/silvestre.hpp
@@ -1,9 +1,18 @@
 #pragma once
 #include "animal.hpp"
+#include <string>
+
+using namespace std;
 
 class Silvestre : public Animal {
 public:
+    Silvestre();
+    Silvestre(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, bool ameacadoDeExtincao);
+    ~Silvestre();
 
-private:
+    bool getAmeacadoDeExtincao() const;
+    void setAmeacadoDeExtincao(bool ameacadoDeExtincao);
 
+protected:
+    bool ameacadoDeExtincao;
 };
