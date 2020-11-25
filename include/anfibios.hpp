f24738400b0e8c02857c112a1c1a3e28c028e509
Anderson Rocha <ironmanrocha81@gmail.com>
2020-11-25 19:30:32 -0300

    Implementaçao dos metodos da classe Anfibios

--- include/anfibios.hpp
+++ include/anfibios.hpp
@@ -8,9 +8,23 @@ class Anfibios : public Animal{
     
 public:
     Anfibios();
-    Anfibios(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao);
+    Anfibios(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, string corDaPele, bool venenoso, bool temPernas);
     ~Anfibios();
 
-private:
+    void saltar();
+    void nadar();
 
+    string getCordaPele() const;
+    void setCorDaPele(string corDaPele);
+
+    bool getVenenoso() const;
+    void setVenenoso(bool venenoso);
+
+    bool getTemPernas() const;
+    void setTemPernas(bool temPernas);
+
+private:
+    string corDaPele;
+    bool venenoso;
+    bool temPernas;
 };
--- src/anfibios.cpp
+++ src/anfibios.cpp
@@ -1,6 +1,43 @@
 #include "anfibios.hpp"
 #include <iostream>
 
-Anfibios::Anfibios(){}
-Anfibios::Anfibios(){}
-Anfibios::~Anfibios(){}
+Anfibios::Anfibios() {}
+Anfibios::Anfibios(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, string corDaPele, bool venenoso, bool temPernas):
+          Animal(especie, sexo, tamanho, cor, preco, tipoDeAlimentacao), corDaPele(corDaPele), venenoso(venenoso), temPernas(temPernas) {}
+Anfibios::~Anfibios() {}
+
+void Anfibios::nadar(){
+    cout << this->especie << " está nadando!" << endl;
+}
+
+void Anfibios::saltar(){
+    if (this->temPernas){
+        cout << this->especie << " está saltando!" << endl;
+    }else{
+        cout << this->especie << " nãao consegue saltar!" << endl;
+    }
+}
+
+string Anfibios::getCordaPele() const{
+    return this->corDaPele;
+}
+
+void Anfibios::setCorDaPele(string corDaPele){
+    this->corDaPele = corDaPele;
+}
+
+bool Anfibios::getVenenoso() const{
+    return this->venenoso;
+}
+
+void Anfibios::setVenenoso(bool venenoso){
+    this->venenoso = venenoso;
+}
+
+bool Anfibios::getTemPernas() const{
+    return this->temPernas;
+}
+
+void Anfibios::setTemPernas(bool temPernas){
+    this->temPernas = temPernas;
+}
