ed836f0e125e6bed5db638596e2a08fde51692f2
Anderson Rocha <ironmanrocha81@gmail.com>
2020-11-25 17:10:56 -0300

    Implementação dos métodos getters e setters da classe Répteis

--- src/repteis.cpp
+++ src/repteis.cpp
@@ -29,3 +29,59 @@ void Repteis::escalar(){
         cout << this->especie << " não consegue escalar!" << endl;
     }
 }
+
+bool Repteis::getTemPernas() const{
+    return this->temPernas;
+}
+
+void Repteis::setTemPernas(bool temPernas){
+    this->temPernas = temPernas;
+}
+
+bool Repteis::getTemNadadeiras() const{
+    return this->temNadadeiras;
+}
+
+void Repteis::setTemNadadeiras(bool temNadadeiras){
+    this->temNadadeiras = temNadadeiras;
+}
+
+bool Repteis::getVenenoso() const{
+    return this->venenoso;
+}
+
+void Repteis::setVenenoso(bool venenoso){
+    this->venenoso = venenoso;
+}
+
+bool Repteis::getPeconhento() const{
+    return this->peconhento;
+}
+
+void Repteis::setPeconhento(bool peconhento){
+    this->peconhento = peconhento;
+}
+
+bool Repteis::getPoeOvos() const{
+    return this->poeOvos;
+}
+
+void Repteis::setPoeOvos(bool poeOvos){
+    this->poeOvos = poeOvos;
+}
+
+bool Repteis::getTemCarapaca() const{
+    return this->temCarapaca;
+}
+
+void Repteis::setTemCarapaca(bool temCarapaca){
+    this->temCarapaca = temCarapaca;
+}
+
+bool Repteis::getConsegueNadar() const{
+    return this->consegueNadar;
+}
+
+void Repteis::setConsegueNadar(bool consegueNadar){
+    this->consegueNadar = consegueNadar;
+}
