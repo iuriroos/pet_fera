613ffd4f4febdeb3e0b724b3bc8dd7fdc9cfe352
Westefns Souza <westefns@outlook.com.br>
2020-11-25 12:38:12 -0300

    Unificação de exótico e nativo na class silvestre

--- include/silvestre.hpp
+++ include/silvestre.hpp
@@ -4,15 +4,28 @@
 
 using namespace std;
 
+enum ETipoSilvestre {
+    Nativo,
+    Exotico
+};
+
 class Silvestre : public Animal {
 public:
     Silvestre();
-    Silvestre(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, bool ameacadoDeExtincao);
+    Silvestre(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, ETipoSilvestre tipoSilvestre, string origem, bool ameacadoDeExtincao);
     ~Silvestre();
+
+    ETipoSilvestre getTipoSilvestre() const;
+    void setTipoSilvestre(ETipoSilvestre tipoSilvestre);
+
+    string getOrigem();
+    void setOrigem(string origem);
 
     bool getAmeacadoDeExtincao() const;
     void setAmeacadoDeExtincao(bool ameacadoDeExtincao);
 
 protected:
+    ETipoSilvestre tipoSilvestre;
+    string origem;
     bool ameacadoDeExtincao;
 };
--- src/silvestre.cpp
+++ src/silvestre.cpp
@@ -2,10 +2,26 @@
 
 Silvestre::Silvestre() {}
 
-Silvestre::Silvestre(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, bool ameacadoDeExtincao)
-    : Animal(especie, sexo, tamanho, cor, preco, tipoDeAlimentacao), ameacadoDeExtincao(ameacadoDeExtincao) {}
+Silvestre::Silvestre(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, ETipoSilvestre tipoSilvestre, string origem, bool ameacadoDeExtincao)
+    : Animal(especie, sexo, tamanho, cor, preco, tipoDeAlimentacao), tipoSilvestre(tipoSilvestre), origem(origem), ameacadoDeExtincao(ameacadoDeExtincao) {}
 
 Silvestre::~Silvestre() {}
+
+ETipoSilvestre Silvestre::getTipoSilvestre() const {
+    return this->tipoSilvestre;
+}
+
+void Silvestre::setTipoSilvestre(ETipoSilvestre tipoSilvestre) {
+    this->tipoSilvestre = tipoSilvestre;
+}
+
+string Silvestre::getOrigem() {
+    return this->origem;
+}
+
+void Silvestre::setOrigem(string origem) {
+    this->origem = origem;
+}
 
 bool Silvestre::getAmeacadoDeExtincao() const {
     return this->ameacadoDeExtincao;
