c7a3e0529efb7784a53601ca334a5911cff0929b
Anderson Rocha <ironmanrocha81@gmail.com>
2020-11-24 23:10:20 -0300

    Criação dos arquivos aves.hpp e aves.cpp

---
+++ include/aves.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include "animal.hpp"
+
+using namespace std;
+
+class Aves : public Animal{  
+public:
+    Aves();
+    Aves(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, bool ameacadoDeExtincao, string corDasPenas, double tamanhoDoBico, double tamanhoDasPenas);
+    ~Aves();
+
+    void voar();
+
+    string getCorDasPenas() const;
+    void setCorDasPenas(string corDasPenas);
+
+    double getTamanhoDoBico() const;
+    void setTamanhoDoBico(double tamanhoDoBico);
+
+    double getTamanhoDasPenas() const;
+    void setTamanhoDasPenas(double tamanhoDasPenas);
+
+private:
+    string corDasPenas;
+    double tamanhoDoBico;
+    double tamanhoDasPenas;
+};
---
+++ src/aves.cpp
@@ -0,0 +1,35 @@
+#include "aves.hpp"
+#include <iostream>
+
+Aves::Aves() {}
+Aves::Aves(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, bool ameacadoDeExtincao, string corDasPenas, double tamanhoDoBico, double tamanhoDasPenas):
+      Animal(especie, sexo, tamanho, cor, preco, tipoDeAlimentacao, ameacadoDeExtincao), corDasPenas(corDasPenas), tamanhoDoBico(tamanhoDoBico), tamanhoDasPenas(tamanhoDasPenas) {}
+Aves::~Aves() {}
+
+void Aves::voar(){
+    cout << "Ave voando!" << endl;
+}
+
+string Aves::getCorDasPenas() const{
+    return this->corDasPenas;
+}
+
+void Aves::setCorDasPenas(string corDasPenas){
+    this->corDasPenas = corDasPenas;
+}
+
+double Aves::getTamanhoDoBico() const{
+    return this->tamanhoDoBico;
+}
+
+void Aves::setTamanhoDoBico(double tamanhoDoBico){
+    this->tamanhoDoBico = tamanhoDoBico;
+}
+
+double Aves::getTamanhoDasPenas() const{
+    return this->tamanhoDasPenas;
+}
+
+void Aves::setTamanhoDasPenas(double tamanhoDasPenas){
+    this->tamanhoDasPenas = tamanhoDasPenas;
+}
