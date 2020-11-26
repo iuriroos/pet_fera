bee812fc965abf26c7568ef19c73c8a2b8f3f29d
Westefns Souza <westefns@outlook.com.br>
2020-11-25 23:27:54 -0300

    correções de reptil silvestre e add de mamiferosil

--- src/mamiferossilvestres.cpp
+++ src/mamiferossilvestres.cpp
@@ -3,7 +3,7 @@
 
 MamiferosSilvestres::MamiferosSilvestres() {}
 MamiferosSilvestres::MamiferosSilvestres(string especie, Sexo sexo, int tamanho, string cor, double preco, string tipoDeAlimentacao, ETipoSilvestre tipoSilvestre, string origem, bool ameacadoDeExtincao, string habitat, Tipo tipo, string corDosPelos, int quantidadeDeDentes, bool temAsas)
-    : Silvestre(especie, sexo, tamanho, cor, preco, tipoDeAlimentacao, tipoSilvestre, origem, ameacadoDeExtincao),
-     Mamiferos(especie,  sexo, tamanho, cor, preco, tipoDeAlimentacao, habitat, tipo, corDosPelos, quantidadeDeDentes, temAsas) {}
+    : Mamiferos(especie,  sexo, tamanho, cor, preco, tipoDeAlimentacao, habitat, tipo, corDosPelos, quantidadeDeDentes, temAsas),
+        Silvestre(especie, sexo, tamanho, cor, preco, tipoDeAlimentacao, tipoSilvestre, origem, ameacadoDeExtincao) {}
 
 MamiferosSilvestres::~MamiferosSilvestres() {}
